c9ecf479f577baf031e1184aca01173b09b2d651
Justin <Jcdiner@gmail.com>
2023-05-05 10:16:07 -0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Sorcerer from './scripts/sorcerer';
-import Castle from './scripts/castle';
 import Game from './scripts/game';
 import LevelOne from './scripts/levels/level_one';
-import { LevelThree } from './scripts/levels/level_three';
 
 export const CANVAS_HEIGHT = 576;
 export const CANVAS_WIDTH = 1024;
@@ -10,58 +8,62 @@ export const CANVAS_WIDTH = 1024;
 document.addEventListener("DOMContentLoaded", () => {
 	const canvas = document.getElementById("canvas");
 	const ctx = canvas.getContext("2d");
-	canvas.height = CANVAS_HEIGHT; // Setting pixel height of canvas. 
-	canvas.width = CANVAS_WIDTH; // Setting pixel width of canvas. 
+	canvas.height = CANVAS_HEIGHT; 
+	canvas.width = CANVAS_WIDTH; 
 	ctx.fillStyle = "gray"; 
 	ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	// Starting Modal Variables
-	const startGameButton = document.getElementById("start_game_button")
+	// Modals and Buttons 
+	const allModals = document.getElementsByClassName('modal');
 	const startingModal = document.getElementById('starting-modal');
-	const allModals = document.getElementsByClassName('modal')
+	const startGameButton = document.getElementById("start_game_button");
+	const replayButton = document.getElementById("replay_button");
+	const losingButton = document.getElementById("losing_button");
 
-	
 	closeAllModals(allModals)
-	// Game Started state flag. 
-	let gameStarted = false;
 
-	// Main game, initial sorcerer, and initial castle variables. 
+	// Main game, initial sorcerer, and initial castle variables. These reset on replay.
+	let gameStarted = false;
 	let levelOne = new LevelOne();
 	let sorcerer = new Sorcerer({x: 180, y: 280});
 	let game = new Game(sorcerer, levelOne.castle);
 
-	const losingModal = document.getElementById("losing-modal");
-	const losingButton = document.getElementById("losing_button")
-
-	losingButton.addEventListener("click", () => {
-		sorcerer = new Sorcerer({x: 180, y: 280});
-		levelOne = new LevelOne();
-		game = new Game(sorcerer, levelOne.castle);
-		closeAllModals(allModals);
-		gameStarted = true;
-	})
-	
-	// Preloads the background. Setting the color of canvas to gray, etc.
-	// Setting the start button on the starting Modal.
-	
+	// Event Listeners for Buttons
 	startGameButton.addEventListener("click", () => {
-		// Once clicked the animate() loop will run and hide display. 
 		closeAllModals(allModals);
 		gameStarted = true;
 		animate();
 	})
 
+	losingButton.addEventListener("click", () => {
+		restartGame();
+	})
+
+	replayButton.addEventListener("click", () => {
+		restartGame();
+	})
+	
+	// Display Initial Start Modal On Game Start
 	startingModal.style.display = "flex";
 
+	// Helper Functions
 	function closeAllModals(modals) {
 		for (let i = 0; i < modals.length; i++) {
 			modals[i].style.display = "none";
 		}
 	}
 
-	// When this is called below, if started, the game animates. 
-	function animate() { 
+	function restartGame() {
+		levelOne = new LevelOne();
+		sorcerer = new Sorcerer({x: 180, y: 280});
+		game = new Game(sorcerer, levelOne.castle);
+		closeAllModals(allModals);
+		gameStarted = true;
+		animate();
+	}
 
+	// Main Animate Function 
+	function animate() { 
 		if (gameStarted) {
 			// If the game.animate() ever returns true, the game is over. 
 			const gameFlag = game.animate(ctx);
@@ -73,7 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		// Repeats the method inside it 60 times per second. 
 		requestAnimationFrame(animate);
 	}
-});
+})
 
 
 
